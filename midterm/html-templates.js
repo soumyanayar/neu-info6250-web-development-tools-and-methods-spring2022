032f42e81aa8c244a4538c7e60022f4cc66f24d4
Soumya Nayar <nayar.s@northeastern.edu>
2022-03-08 17:53:18 -0800

    Styling changes and extra credit requirements

--- midterm/html-templates.js
+++ midterm/html-templates.js
@@ -12,9 +12,7 @@ const loginPage = () => {
         <div class="login-container">
           <div id="content" class="content-container">
             <form action="/login" method="POST"> 
-              <div class="page-title">
-                <h1>Word Guessing Game</h1>
-              </div>
+                <h1>Word Guessing Game</h1>    
               <div class="text-input" id="login_username">
                 <input name="username" placeholder="Enter your username" required> 
                 <div class="space">
@@ -35,7 +33,7 @@ const invalidUserNameHtml = (errorMessage) => {
     <h2>${errorMessage}</h2>
     <h3>Please Login Again..!!</h3>
     <form method="GET" action="/">
-    <button type="submit">Login</button>
+    <button class="login-button" type="submit">Login</button>
     </form>
   `;
 };
@@ -52,54 +50,60 @@ const homePage = (user) => {
       <div id="word-guessing-game">
         <div class="page-title">
             <h2>Word Guessing Game, Welcome ${user.username} </h2>
-        </div>
-        <div class="game-panel">
-          <div class="accepted-guess-panel">
-            <h2>Guess & Matches History</h2>
-            <div class="word-list">
-                ${user.game.guessedWords
-                  .map(
-                    (guess) =>
-                      `<div><span class="word">${guess.guessedWord}</span> : <span class="word">${guess.numberOfMatchingLetters}</span></div>`
-                  )
-                  .join("")}
-            </div>
-            <div class="turns">
-              Number Of Accepted Guesses: ${user.game.numberOfValidGuesses}
-            </div>
-          </div>
-         
-          <div class="control-panel">
-            <h2>Control Panel</h2>
-            <div class="word-input">
-              <form action="/guess" method="POST"> 
-                <input name="guessedWord" placeholder="Type your guess"> 
-                <button class="guess-button" type="submit">GUESS</button>
+            <div class="logout"> 
+              <form action="/logout" method="POST"> 
+                <button class="logout-button" type="submit">LOGOUT</button>
               </form>
             </div>
-            <div class="controls">
+        </div>
+        <div class="game-panel">
+        <div class="game-panel-container">
+          <div class="control-panel">
+            <div class="word-input">
+              <form action="/guess" method="POST"> 
+                <input name="guessedWord" placeholder="Type your guess" required ${
+                  user.game.isGameWon ? "disabled" : "enabled"
+                }> 
+                <button class="guess-button" type="submit" ${
+                  user.game.isGameWon ? "disabled" : "enabled"
+                }>GUESS</button>
+              </form>
+              <div class="message-panel">
+              ${user.game.message}
+              </div> 
+              <div class="controls">
               <div class="restart"> 
                 <form action="/restart" method="GET"> 
                   <button class="restart-button" type="submit">RESTART</button>
                 </form>
               </div>
-              <div class="logout"> 
-                <form action="/logout" method="POST"> 
-                  <button class="logout-button" type="submit">LOGOUT</button>
-                </form>
+            </div>
+          </div>
+        </div>
+            <div class="accepted-guess-panel">
+              <h3>Your Guesses & Letter Match History</h3>
+              <div class="word-list">
+                  ${user.game.guessedWords
+                    .map(
+                      (guess) =>
+                        `<div><span class="word">You guessed "${guess.guessedWord}"</span> : <span class="word">matched ${guess.numberOfMatchingLetters} letters with secret word</span></div>`
+                    )
+                    .join("")}
               </div>
-            </div> 
-            <div class="message-panel">
-              ${user.game.message}
-            </div> 
+              <div class="turns">
+                Number Of Accepted Guesses: ${user.game.numberOfValidGuesses}
+              </div>
+            </div>
           </div>
           <div class="word-list-panel">
-          <h2>Secret Word List</h2>
-          <span class="word-list">
-            ${words.map((word) => `<span class="word">${word}</span>`).join("")}
-          </span>
-        </div>
-        </div>
+            <h3>Secret Word List</h3>
+            <div class="word-list">
+              ${words
+                .map((word) => `<span class="word">${word}</span>`)
+                .join("")}
+            </div>
+          </div>
+        </div> 
       </div>
     </body>
   </html>
