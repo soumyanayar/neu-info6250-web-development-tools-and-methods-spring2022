56a9223258978534c8d737f78a0abcc06d1ce41d
Soumya Nayar <nayar.s@northeastern.edu>
2022-04-30 23:12:58 +0000

    Add Logs Accordian

--- final/client/src/Components/home-Pages/AddHabitLogAccordian.jsx
+++ final/client/src/Components/home-Pages/AddHabitLogAccordian.jsx
@@ -1,32 +1,155 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+import {
+    fetchGetSingleHabit,
+    fetchPostCreateGoodHabitLog,
+    fetchPostLimitBadHabitLog,
+    fetchPostQuitBadHabitLog,
+} from "../../services/habitservices";
 
-function AddHabitLogAccordian({title}) {
-  const [isEntryOpen, setIsEntryOpen] = useState({});
-
-  function toggleEntry(title) {
-    setIsEntryOpen({
-      ...isEntryOpen,
-      [title]: !isEntryOpen[title],
+function AddHabitLogAccordian({ habitId, habitType }) {
+    const [isEntryOpen, setIsEntryOpen] = useState({});
+    const [habit, setHabit] = useState({
+        startDate: new Date().toISOString().slice(0, 10),
     });
-  }
-  const isOpen = isEntryOpen[title];
-  return (
-    <div className="accordion">
-          <div
-            key={title}
-            className={`accordion__entry ${isOpen ? 'accordion__entry--open' : '' }`}
-          >
-            <button
-              className="accordion__title"
-              onClick={ () => toggleEntry(title) }
+    const [error, setError] = useState("");
+    const [habitLogNumber, setHabitLogNumber] = useState(0);
+    const [habitLogDate, setHabitLogDate] = useState(
+        new Date().toISOString().slice(0, 10)
+    );
+    const [habitLogIsSuccess, setHabitLogIsSuccess] = useState(false);
+    const [habitUnit, setHabitUnit] = useState("times");
+
+    function toggleEntry(habitId) {
+        setIsEntryOpen({
+            ...isEntryOpen,
+            [habitId]: !isEntryOpen[habitId],
+        });
+    }
+    const isOpen = isEntryOpen[habitId];
+
+    const getHabitDetails = async () => {
+        try {
+            const fetchedHabit = await fetchGetSingleHabit(habitId);
+            setHabit(fetchedHabit);
+            if (
+                habitType === "CreateGoodHabit" ||
+                habitType === "LimitBadHabit"
+            ) {
+                setHabitUnit(fetchedHabit.unit);
+            }
+            setError("");
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            switch (habitType) {
+                case "CreateGoodHabit":
+                    await fetchPostCreateGoodHabitLog(
+                        habitId,
+                        habitLogNumber,
+                        habitUnit,
+                        new Date(habitLogDate)
+                    );
+                    break;
+
+                case "LimitBadHabit":
+                    await fetchPostLimitBadHabitLog(
+                        habitId,
+                        habitLogNumber,
+                        habitUnit,
+                        new Date(habitLogDate)
+                    );
+                    break;
+
+                case "QuitBadHabit":
+                    await fetchPostQuitBadHabitLog(
+                        habitId,
+                        habitLogIsSuccess,
+                        new Date(habitLogDate)
+                    );
+                    break;
+
+                default:
+                    console.log("Invalid habitType");
+                    break;
+            }
+            setError("");
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
+    useEffect(() => {
+        getHabitDetails();
+    }, []);
+
+    return (
+        <div className="accordion">
+            <div
+                key={habitId}
+                className={`accordion__entry ${
+                    isOpen ? "accordion__entry--open" : ""
+                }`}
             >
-                View
-            </button>
-            <div className="accordion__body">
+                <button
+                    className="accordion__title"
+                    onClick={() => toggleEntry(habitId)}
+                >
+                    Add a Log for this Habit
+                </button>
+                <div className="accordion__body">
+                    <form>
+                        {(habitType === "CreateGoodHabit" ||
+                            habitType === "LimitBadHabit") && (
+                            <div className="habit-data-3">
+                                <input
+                                    type="number"
+                                    value={habitLogNumber}
+                                    placeholder="2"
+                                    onChange={(e) =>
+                                        setHabitLogNumber(e.target.value)
+                                    }
+                                ></input>
+                                <div>{habitUnit}</div>
+                                <input
+                                    type="date"
+                                    value={habitLogDate}
+                                    onChange={(e) =>
+                                        setHabitLogDate(e.target.value)
+                                    }
+                                ></input>
+                            </div>
+                        )}
+
+                        {habitType === "QuitBadHabit" && (
+                            <div className="habit-data-3">
+                                <input
+                                    type="date"
+                                    value={habitLogDate}
+                                    onChange={(e) =>
+                                        setHabitLogDate(e.target.value)
+                                    }
+                                ></input>
+                            </div>
+                        )}
+
+                        <button
+                            className="save-btn"
+                            type="submit"
+                            onClick={handleSubmit}
+                        >
+                            Save Log!
+                        </button>
+                    </form>
+                </div>
             </div>
-          </div>
-    </div>
-  );
-};
+            {error && <span className="error-field">{error}</span>}
+        </div>
+    );
+}
 
 export default AddHabitLogAccordian;
--- final/client/src/Components/home-Pages/Home.jsx
+++ final/client/src/Components/home-Pages/Home.jsx
@@ -1,56 +1,66 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import ViewLog from "./ViewLog";
 import { fetchGetAllHabits } from "../../services/habitservices";
-import ViewHabitDetailsAccordian from './ViewHabitDetailsAccordian';
+import ViewHabitDetailsAccordian from "./ViewHabitDetailsAccordian";
+import AddHabitLogAccordian from "./AddHabitLogAccordian";
 
 const Home = () => {
-  const [allHabits, setAllHabits] = useState({});
-  const [habit, setHabit] = useState({});
-  const [error, setError] = useState("");
+    const [allHabits, setAllHabits] = useState({});
+    const [habit, setHabit] = useState({});
+    const [error, setError] = useState("");
 
-  const getAllHabits = async () => {
-    try {
-      const fetchedHabitIds = await fetchGetAllHabits();
-      setAllHabits(fetchedHabitIds);
-      setError("");
-    } catch (error) {
-      setError(error.message);
+    const getAllHabits = async () => {
+        try {
+            const fetchedHabitIds = await fetchGetAllHabits();
+            setAllHabits(fetchedHabitIds);
+            setError("");
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    useEffect(() => {
+        getAllHabits();
+    }, []);
+
+    if (allHabits.length === 0) {
+        return (
+            <div>
+                <h1>Welcome to Habit Tracker</h1>
+                <p>
+                    To get started, create a new habit by clicking{" "}
+                    <Link>Add habit</Link>
+                </p>
+            </div>
+        );
     }
-  };
-
-  useEffect(() => {
-    getAllHabits();
-  }, []);
-
-  if (allHabits.length === 0) {
     return (
-      <div>
-        <h1>Welcome to Habit Tracker</h1>
-        <p>
-          To get started, create a new habit by clicking <Link>Add habit</Link>
-        </p>
-      </div>
+        <div>
+            <h3> You have below habits so far !!</h3>
+            <div>
+                <ul>
+                    {Object.entries(allHabits).map(([key, value]) => {
+                        return (
+                            <li key={key}>
+                                <span>
+                                    <b>{value.habitName}</b>
+                                </span>
+                                <ViewHabitDetailsAccordian
+                                    habitId={value.habitId}
+                                    habitType={value.habitType}
+                                />
+                                <AddHabitLogAccordian
+                                    habitId={value.habitId}
+                                    habitType={value.habitType}
+                                />
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
     );
-  }
-  return (
-    <div>
-      <h3> You have below habits so far !!</h3>
-      <div>
-        <ul>
-          {Object.entries(allHabits).map(([key, value]) => {
-            return (
-              <li key={key}>
-                <span><b>{value.habitName}</b></span>
-                <ViewHabitDetailsAccordian habitId={value.habitId} habitType={value.habitType}/>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
 };
 
 export default Home;
