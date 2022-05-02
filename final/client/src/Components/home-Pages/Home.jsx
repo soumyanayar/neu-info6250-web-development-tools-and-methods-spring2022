ea78a03867b37258b30cd96cfa5b84e518e40fd3
Soumya Nayar <nayar.s@northeastern.edu>
2022-05-01 22:06:59 -0700

    Delete btn for sigle habit

--- final/client/src/Components/home-Pages/Home.jsx
+++ final/client/src/Components/home-Pages/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { fetchGetAllHabits } from "../../services/habitservices";
+import {
+  fetchGetAllHabits,
+  fetchDeleteSingleHabit,
+} from "../../services/habitservices";
 import ViewHabitDetailsAccordian from "./ViewHabitDetailsAccordian";
 import AddHabitLogAccordian from "./AddHabitLogAccordian";
-import DeleteSingleModal from "../modals/DeleteSingleHabitModal";
 
 const Home = () => {
   const [allHabits, setAllHabits] = useState({});
   const [error, setError] = useState("");
-  const [deleteHabitModalOpen, setDeleteHabitModalOpen] = useState(false);
-  const [habitKey, setHabitKey] = useState(0);
+  const [updated, setUpdated] = useState(false);
 
   const getAllHabits = async () => {
     try {
@@ -22,14 +23,18 @@ const Home = () => {
     }
   };
 
-  const handleDelete = (habitKey) => {
-    setHabitKey(habitKey);
-    setDeleteHabitModalOpen(true);
+  const handleDeleteSingleHabit = async (key) => {
+    try {
+      await fetchDeleteSingleHabit(key);
+      setUpdated(!updated);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     getAllHabits();
-  }, [deleteHabitModalOpen]);
+  }, [updated]);
 
   if (allHabits.length === 0) {
     return (
@@ -45,27 +50,13 @@ const Home = () => {
     <div>
       <p className="home-title"> You have below habits so far !!</p>
       <div className="all-habit-container">
-        {deleteHabitModalOpen && (
-          <DeleteSingleModal
-            habitKey={habitKey}
-            setDeleteHabitModalOpen={setDeleteHabitModalOpen}
-          />
-        )}
         <ul className="all-habit-list">
           {Object.entries(allHabits).map(([key, value]) => {
             return (
               <li key={key}>
                 <div className="habit-container-div">
                   <p className="habit-name-title">
-                    <b>
-                      {value.habitName}{" "}
-                      <button
-                        className="delete-habit-btn"
-                        onClick={() => handleDelete({ key })}
-                      >
-                        X
-                      </button>
-                    </b>
+                    <b>{value.habitName} </b>
                   </p>
                   <div className="accordion-holder-div">
                     <ViewHabitDetailsAccordian
@@ -77,6 +68,12 @@ const Home = () => {
                       habitType={value.habitType}
                     />
                   </div>
+                  <button
+                    className="delete-habit-btn"
+                    onClick={() => handleDeleteSingleHabit(key)}
+                  >
+                    DELETE HABIT
+                  </button>
                 </div>
               </li>
             );
