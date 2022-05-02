cd74e6326868a8a9e35069e6258bfa65ef771e47
Soumya Nayar <nayar.s@northeastern.edu>
2022-05-01 23:11:52 -0700

    Pie styling

--- final/client/src/Components/home-Pages/Piechart.jsx
+++ final/client/src/Components/home-Pages/Piechart.jsx
@@ -1,39 +1,51 @@
 import { useState } from "react";
 
 const Piechart = ({ data }) => {
-    const mystyle = {
-        margin: "20px auto",
-        padding: "25px",
-        maxWidth: "200px",
-        height: "200px",
-        borderRadius: "50%",
-        boxShadow:
-            "0 3px 1px 2px rgba(0, 0, 0, 0.2),0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12)",
-        backgroundImage:
-            "conic-gradient(magenta " +
-            data.degree1 +
-            "deg, magenta " +
-            data.degree2 +
-            "deg, cyan " +
-            data.degree2 +
-            "deg, cyan " +
-            data.degree3 +
-            "deg, yellow " +
-            data.degree3 +
-            "deg, yellow " +
-            data.degree4 +
-            "deg, green " +
-            data.degree4 +
-            "deg, green " +
-            data.degree5 +
-            "deg)",
-    };
+  const mystyle = {
+    margin: "20px auto",
+    padding: "6rem",
+    maxWidth: "200px",
+    height: "200px",
+    borderRadius: "50%",
+    // boxShadow:
+    //   "0 3px 1px 2px rgba(0, 0, 0, 0.2),0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12)",
+    backgroundImage:
+      "conic-gradient(limegreen " +
+      data.degree1 +
+      "deg, limegreen " +
+      data.degree2 +
+      "deg, coral " +
+      data.degree2 +
+      "deg, coral " +
+      data.degree3 +
+      "deg, lightblue " +
+      data.degree3 +
+      "deg, lightblue " +
+      data.degree4 +
+      "deg, pink " +
+      data.degree4 +
+      "deg, pink " +
+      data.degree5 +
+      "deg)",
+  };
 
-    return (
-        <div>
-            <div style={mystyle}></div>
+  return (
+    <div className="pie-chart-container">
+      <div style={mystyle}></div>
+      <div>
+        <p>Legend</p>
+        <div className="limegreen-div">
+          <button></button> <span>Successful</span>
         </div>
-    );
+        <div className="coral-div">
+          <button></button> <span>Failed</span>
+        </div>
+        <div className="coral-div">
+          <button></button> <span>Failed</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Piechart;
